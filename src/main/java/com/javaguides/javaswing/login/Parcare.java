923a98d0bfca7554f952e156f75ef738b2b1bf76
User <raresadrians@yahoo.com>
2021-05-19 17:35:45 +0300

    some commits

---
+++ src/main/java/com/javaguides/javaswing/login/Parcare.java
@@ -0,0 +1,17 @@
+package com.javaguides.javaswing.login;
+
+public class Parcare {
+    static String nume;
+
+    //static String parola;
+    //static String companie;
+    public Parcare(String a) {
+        nume = a;
+        //parola=b;
+        //companie=c;
+    }
+
+    public Parcare() {
+
+    }
+}
